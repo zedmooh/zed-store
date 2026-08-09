1018b93c59154a1b8d1501260146d35ea6891309
zedmooh <zedmooh1980@gmail.com>
2026-08-09 21:46:38 +0100

    Auto update Cloudflare Tunnel URL: https://terminology-obvious-horn-stuart.trycloudflare.com

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 const API_BASE = (typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'))
   ? 'http://localhost:4000'
-  : 'https://donna-kijiji-con-wars.trycloudflare.com';
+  : 'https://terminology-obvious-horn-stuart.trycloudflare.com';
 
 let db = null;
 if (Object.keys(firebaseConfig).length > 0 && firebaseConfig.apiKey) {
