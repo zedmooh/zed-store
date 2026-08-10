2d1a7eec14361659fe381b42b63854daf527fc5d
zedmooh <zedmooh1980@gmail.com>
2026-08-10 20:20:56 +0100

    Auto update Cloudflare Tunnel URL: https://embedded-components-scroll-recommendation.trycloudflare.com

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 const API_BASE = (typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'))
   ? 'http://localhost:4000'
-  : 'https://convenient-robertson-skirt-property.trycloudflare.com';
+  : 'https://embedded-components-scroll-recommendation.trycloudflare.com';
 
 let db = null;
 if (Object.keys(firebaseConfig).length > 0 && firebaseConfig.apiKey) {
