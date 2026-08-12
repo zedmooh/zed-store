182fd3c5b2c9cbca74a166ad5b72b5dadd706b9f
zedmooh <zedmooh1980@gmail.com>
2026-08-12 12:01:15 +0100

    Auto update Cloudflare Tunnel URL: https://cloudy-carrying-dist-petroleum.trycloudflare.com

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 const API_BASE = (typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'))
   ? 'http://localhost:4000'
-  : 'https://smtp-oaks-administrative-relate.trycloudflare.com';
+  : 'https://cloudy-carrying-dist-petroleum.trycloudflare.com';
 
 let db = null;
 if (Object.keys(firebaseConfig).length > 0 && firebaseConfig.apiKey) {
